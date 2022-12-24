37890ab2e1144f79fe961c76dd4cb2e03c6fd923
omar saadeh <omarsaadeh11@gmail.com>
2022-12-24 04:38:34 -0800

    Transactions

--- routes/rentals.js
+++ routes/rentals.js
@@ -10,8 +10,12 @@ const { Rental, validate } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const { Customer } = require("../models/customer");
 const mongoose = require("mongoose");
+// class
+const Fawn = require("fawn");
 const express = require("express");
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get("/", async (req, res) => {
   // -   yaane kbir lal sgir
@@ -44,8 +48,56 @@ router.post("/", async (req, res) => {
       dailyRentalRate: movie.dailyRentalRate,
     },
   });
+  // rental = await rental.save();
+
+  // movie.numberInStock--;
+  // movie.save();
+
+  try {
+    new Fawn.Task()
+      .save("rentals", rental)
+      .update(
+        "movies",
+        { _id: movie._id },
+        {
+          $inc: { numberInStock: -1 },
+        }
+      )
+      // .remove()
+      .run();
+    res.send(rental);
+  } catch (error) {
+    res.status(500).send("Something failed");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const rental = await Rental.findById(req.params.id);
+
+  if (!rental)
+    return res.status(404).send("The rental with the given ID was not found.");
+
+  res.send(rental);
+});
+
+module.exports = router;
+
+/*
+
+
+
+
+
   //1
   rental = await rental.save();
+  //bel relational database fi 3ana el concept of transactions
+  //but in mongodb we dont have transaction...
+  // there is technique that called
+  // two phase commit which is beyond the scope of this course
+  //thats an advanced topic related to mongodb
+  // so lets see a npm package that simulate a transaction in mongoose.
+  //
+  //
   // here we could have a problem..we have 2 separate operations
   // its possible that after we save the rental something goes wrong
   //maybe our server crushes or connection to mongodb drops.
@@ -63,16 +115,5 @@ router.post("/", async (req, res) => {
   //2
   movie.save();
 
-  res.send(rental);
-});
 
-router.get("/:id", async (req, res) => {
-  const rental = await Rental.findById(req.params.id);
-
-  if (!rental)
-    return res.status(404).send("The rental with the given ID was not found.");
-
-  res.send(rental);
-});
-
-module.exports = router;
+*/
